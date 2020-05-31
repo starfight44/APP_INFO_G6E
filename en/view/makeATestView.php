f316df291dcb133ee781f1325f6ade14b15752da
HugoRialan <hugo.rialan@isep.fr>
2020-05-31 10:19:14 +0200

    corrections

--- en/view/makeATestView.php
+++ en/view/makeATestView.php
@@ -3,7 +3,7 @@ ob_start(); ?>
 
     <section><h2>Make a test :</h2></section>
     <section>
-            <p align="center" class="warning"><strong>INSTRUCTIONS :</strong> click on a test to delete it<br>Add the tests in the order in which you want to run them</p>
+            <p align="center" class="warning"><strong>INSTRUCTIONS :</strong> Add up to 5 tests in the list below, click on a test to delete it <br> Adjust the different sensors on your body before running the test</p>
             <br><br>
         <div id="addTest">
             <form action="index.php?action=addTest" method="POST">
--- view/makeATestView.php
+++ view/makeATestView.php
@@ -3,7 +3,7 @@ ob_start(); ?>
 
     <section><h2>Faire un test :</h2></section>
     <section>
-            <p align="center" class="warning"><strong>INSTRUCTIONS :</strong> cliquez sur un test pour le supprimer<br>Ajoutez les tests dans l'ordre dans lesquels vous voulez les executer</p>
+            <p align="center" class="warning"><strong>INSTRUCTIONS :</strong> Ajoutez jusqu'à 5 tests dans la liste ci dessous, cliquez sur un test pour le supprimer<br>Ajustez les différents capteurs sur votre corps avant d'éxecuter le test</p>
             <br><br>
         <div id="addTest">
             <form action="index.php?action=addTest" method="POST">
